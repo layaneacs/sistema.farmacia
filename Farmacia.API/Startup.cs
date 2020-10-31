9e187717fc1a56879124cabab743281982f36455
layane.andreia@hotmail.com <layane.andreia@hotmail.com>
2020-10-30 22:47:15 -0300

    Adicionado Swagger - Documentacao

--- Farmacia.API/Startup.cs
+++ Farmacia.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Farmacia.API
 {
@@ -25,12 +26,18 @@ namespace Farmacia.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c => 
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Farma RL API" , Version = "v1"}));
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Farma RL API"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
